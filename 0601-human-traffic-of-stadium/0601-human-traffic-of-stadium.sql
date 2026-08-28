f9472a82181dd3c03d0bac1389c75a61272c2fae
Nilay Bhamare <nilaybhamareclg@gmail.com>
2026-08-28 15:24:56 +0530

    Time: 328 ms (99.6%), Space: 0B (100%) - LeetHub

--- 0601-human-traffic-of-stadium/0601-human-traffic-of-stadium.sql
+++ 0601-human-traffic-of-stadium/0601-human-traffic-of-stadium.sql
@@ -1,15 +1,35 @@
-WITH high_traffic AS (
-    SELECT *,
-           id - ROW_NUMBER() OVER (ORDER BY id) AS grp
-    FROM Stadium
-    WHERE people >= 100
-)
-SELECT id, visit_date, people
-FROM high_traffic
-WHERE grp IN (
-    SELECT grp
-    FROM high_traffic
-    GROUP BY grp
-    HAVING COUNT(*) >= 3
-)
+SELECT DISTINCT s.*
+FROM Stadium s
+JOIN Stadium s1
+    ON s1.id = s.id + 1
+JOIN Stadium s2
+    ON s2.id = s.id + 2
+WHERE s.people >= 100
+  AND s1.people >= 100
+  AND s2.people >= 100
+
+UNION
+
+SELECT DISTINCT s.*
+FROM Stadium s
+JOIN Stadium s1
+    ON s1.id = s.id + 1
+JOIN Stadium s2
+    ON s2.id = s.id - 1
+WHERE s.people >= 100
+  AND s1.people >= 100
+  AND s2.people >= 100
+
+UNION
+
+SELECT DISTINCT s.*
+FROM Stadium s
+JOIN Stadium s1
+    ON s1.id = s.id - 1
+JOIN Stadium s2
+    ON s2.id = s.id - 2
+WHERE s.people >= 100
+  AND s1.people >= 100
+  AND s2.people >= 100
+
 ORDER BY visit_date;
